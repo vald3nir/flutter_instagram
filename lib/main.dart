9fec341228dc7ef881af80a9e9013f2682bbeaec
vald3nir <vald3nir@gmail.com>
2022-01-22 11:48:50 -0300

    setup firebase [web]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram/utils/colors.dart';
 
@@ -8,7 +9,19 @@ import 'responsive/web_screen_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCMnLQ7UsZUP0WxX1wFjPihMJAIx0BeCIs",
+          authDomain: "flutter-instagram-clone-b8837.firebaseapp.com",
+          projectId: "flutter-instagram-clone-b8837",
+          storageBucket: "flutter-instagram-clone-b8837.appspot.com",
+          messagingSenderId: "1097969548885",
+          appId: "1:1097969548885:web:2a3718398e58b3a2a3dedc"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
